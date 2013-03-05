cee8ef1f09ac17578f6f0a824141a381cefc1c61
Brendon Jones <brendonj@waikato.ac.nz>
2013-03-05 02:39:37 +0000

    Fix indentation so that appropriate boolean values can be parsed.

--- lib/configurator.py
+++ lib/configurator.py
@@ -35,7 +35,7 @@ def get_nntsc_config_bool(nntsc_config, section, option):
     except ConfigParser.ValueError:
         print >> sys.stderr, "The option '%s' in section '%s' does not have a boolean value" % (option, section)
         print >> sys.stderr, "Suitable values are 'true', '1', 'on', 'yes', 'false', 0, 'off', or 'no'"
-    return "NNTSCConfigError"
+        return "NNTSCConfigError"
 
     return result
 
