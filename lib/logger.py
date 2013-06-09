528a305514f30baa8df12a3319a31b2921c20e35
Shane Alcock <salcock@waikato.ac.nz>
2013-06-09 23:43:45 +0000

    * Made the logging actually work!  * The createLogger function will now create a log file at a specified location and use a given name for the daemon  * Updated datacollector to use the new logger functionality  * All dataparser processes are now created as daemon processes -- this means they will disappear if the datacollector is killed. Much easier than trying to clean up manually.

--- lib/logger.py
+++ lib/logger.py
@@ -4,25 +4,26 @@ import logging
 backgrounded = False
 logger = None
 
-def setBackgrounded(flag):
+def createLogger(flag, logfile, name):
+    global backgrounded, logger
     backgrounded = flag
 
     if backgrounded:
-        #logger = logging.getLogger("NNTSCLogger")
-        #logger.setLevel(logging.DEBUG)
-        #formatter = logging.Formatter("(asctime)s - %(name)s - %(levelname)s - %(message)s")
-        #handler = logging.FileHandler("/tmp/nntsc.log")
-        #handler.setFormatter(formatter)
-        #logger.addHandler(handler)
+        logger = logging.getLogger(name)
+        logger.setLevel(logging.DEBUG)
+        formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+        handler = logging.FileHandler(logfile)
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
 
-        logging.basicConfig(file="/tmp/nntsc.log", format="(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.DEBUG)
+        #logging.basicConfig(filename="/tmp/nntsc.log", format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
     
 def log(msg):
 
     if not backgrounded:
         print >> sys.stderr, msg
     else:
-        logging.debug(msg) 
+        logger.debug(msg) 
 
 
 # vim: set sw=4 tabstop=4 softtabstop=4 expandtab :
