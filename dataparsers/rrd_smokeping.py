8ee5547c4b306e9accf8d2a787dbdaa44463a500
Shane Alcock <salcock@waikato.ac.nz>
2014-03-28 14:10:32 +1300

    Convert ping columns into a single array column in rrd smokeping
    This will be much more flexible when working with other people's
    smokeping setups (which may use a different number of pings).
    
    It also makes working with the individual pings much easier.

--- dataparsers/rrd_smokeping.py
+++ dataparsers/rrd_smokeping.py
@@ -24,6 +24,8 @@ from sqlalchemy import create_engine, Table, Column, Integer, \
 from sqlalchemy.types import Integer, String, Float, SmallInteger
 from sqlalchemy.exc import IntegrityError, OperationalError, ProgrammingError,\
         SQLAlchemyError, DataError
+from sqlalchemy.dialects import postgresql
+from sqlalchemy.sql import text
 import libnntscclient.logger as logger
 from libnntsc.database import DB_NO_ERROR, DB_GENERIC_ERROR, DB_DATA_ERROR
 
@@ -65,28 +67,8 @@ def data_table(db):
                 nullable = False),
         Column('timestamp', Integer, nullable=False),
         Column('loss', SmallInteger, nullable=True),
-        Column('uptime', Float, nullable=True),
         Column('median', Float, nullable=True),
-        Column('ping1', Float, nullable=True),
-        Column('ping2', Float, nullable=True),
-        Column('ping3', Float, nullable=True),
-        Column('ping4', Float, nullable=True),
-        Column('ping5', Float, nullable=True),
-        Column('ping6', Float, nullable=True),
-        Column('ping7', Float, nullable=True),
-        Column('ping8', Float, nullable=True),
-        Column('ping9', Float, nullable=True),
-        Column('ping10', Float, nullable=True),
-        Column('ping11', Float, nullable=True),
-        Column('ping12', Float, nullable=True),
-        Column('ping13', Float, nullable=True),
-        Column('ping14', Float, nullable=True),
-        Column('ping15', Float, nullable=True),
-        Column('ping16', Float, nullable=True),
-        Column('ping17', Float, nullable=True),
-        Column('ping18', Float, nullable=True),
-        Column('ping19', Float, nullable=True),
-        Column('ping20', Float, nullable=True),
+        Column('pings', postgresql.ARRAY(Float), nullable=True),
         useexisting=True
     )
 
@@ -104,33 +86,33 @@ def insert_stream(db, exp, name, fname, source, host, minres, rows):
 
 
 def insert_data(db, exp, stream, ts, line):
-    # This is terrible :(
-
     kwargs = {}
-    exportdict = {}
-    line_map = {0:"uptime", 1:"loss", 2:"median", 3:"ping1", 4:"ping2",
-        5:"ping3", 6:"ping4", 7:"ping5", 8:"ping6", 9:"ping7", 10:"ping8",
-        11:"ping9", 12:"ping10", 13:"ping11", 14:"ping12", 15:"ping13",
-        16:"ping14", 17:"ping15", 18:"ping16", 19:"ping17", 20:"ping18",
-        21:"ping19", 22:"ping20"}
 
-    for i in range(0, len(line)):
+    if len(line) >= 1:
+        kwargs['loss'] = int(float(line[1]))
+
+    if len(line) >= 2:
+        kwargs['median'] = round(float(line[2]) * 1000.0, 6)
+        
+    kwargs['pings'] = []
+
+    for i in range(3, len(line)):
         if line[i] == None:
             val = None
-        elif i == 1:
-            val = int(float(line[i]))
-        elif i > 1:
-            val = round(float(line[i]) * 1000.0, 6)
         else:
-            val = round(float(line[i]), 6)
+            val = round(float(line[i]) * 1000.0, 6)
 
-        if val != None:
-            kwargs[line_map[i]] = val
+        kwargs['pings'].append(val)
 
-        exportdict[line_map[i]] = val
+    
+    insertfunc = text("INSERT INTO %s ("
+            "stream_id, timestamp, loss, median, pings) VALUES ("
+            ":stream_id, :timestamp, :loss, :median, "
+            "CAST(:pings AS double precision[]))" % \
+            (DATA_TABLE_NAME + "_" + str(stream)))
 
     return db.insert_data(exp, DATA_TABLE_NAME, "rrd_smokeping", stream, ts, 
-            exportdict)
+            kwargs, insertfunc)
 
 
 # vim: set sw=4 tabstop=4 softtabstop=4 expandtab :
