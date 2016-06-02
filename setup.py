2d5df2d03ca4e293b4ccfabd96d45dd2ecaca4b9
Shane Alcock <salcock@waikato.ac.nz>
2016-06-02 17:26:39 +1200

    Bump version number in setup.py

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ if sys.version_info < (2, 7):
         requires.append('argparse')
 
 setup(name="nntsc",
-	version="2.8",
+	version="2.9",
 	description='Nathan\'s Network Time Series Collector',
         author='Nathan Overall, Shane Alcock',
         author_email='contact@wand.net.nz',
