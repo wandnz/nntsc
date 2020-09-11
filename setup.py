ccfe46eeeeb713cf910050cecd762b40d74b2b7d
Brendon Jones <brendonj@waikato.ac.nz>
2020-09-11 17:33:06 +1200

    Bump version number to 2.24 and update changelog.

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ if sys.version_info < (2, 7):
         requires.append('argparse')
 
 setup(name="nntsc",
-	version="2.23",
+	version="2.24",
 	description='Nathan\'s Network Time Series Collector',
         author='Shane Alcock, Brendon Jones',
         author_email='contact@wand.net.nz',
